6627f2801ae4dcd391bfb0e857a9972555f5b1d0
PROJECT <rlawkaud11@naver.com>
2018-10-06 18:11:45 +0900

    JM 1006 MainActivity.java --> 메인화면 틀 + 하단 Tab동작 SigninActivity.java --> 새로 작성, signin.xml 구현위함
    activity_main.xml --> 중간 스와이프 구현 x

--- app/src/main/java/com/example/jmkim/nomad/MainActivity.java
+++ app/src/main/java/com/example/jmkim/nomad/MainActivity.java
@@ -1,13 +1,74 @@
 package com.example.jmkim.nomad;
 
+import android.app.Activity;
+import android.app.LocalActivityManager;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TabHost;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    TabHost tabHost;
+    LocalActivityManager mLocalActivityManager;
+    Button btn_sign, btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initTabs(savedInstanceState);
+
+        btn_sign=(Button)findViewById(R.id.main_btn_sign);
+        btn_login=(Button)findViewById(R.id.main_btn_login);
+
+        btn_sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),SigninActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        mLocalActivityManager.dispatchResume();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        mLocalActivityManager.dispatchPause(isFinishing());
+        super.onPause();
+    }
+
+    private void initTabs(Bundle savedInstanceState){ //Tab사용하기 위함
+        Resources res=getResources();
+        tabHost=(TabHost)findViewById(android.R.id.tabhost);
+        mLocalActivityManager=new LocalActivityManager(this,false);
+        mLocalActivityManager.dispatchCreate(savedInstanceState);
+        tabHost.setup(mLocalActivityManager);
+
+        TabHost.TabSpec spec;
+        spec=tabHost.newTabSpec("Join").setIndicator("파티")
+                .setContent(R.id.main_tab_join);
+        tabHost.addTab(spec);
+
+        spec=tabHost.newTabSpec("Review").setIndicator("후기")
+                .setContent(R.id.main_tab_review);
+        tabHost.addTab(spec);
+
+        spec=tabHost.newTabSpec("Free").setIndicator("자유")
+                .setContent(R.id.main_tab_free);
+        tabHost.addTab(spec);
+
+        tabHost.setCurrentTab(0);
     }
 }
